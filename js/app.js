c57db23ad9466b1241072a5bba9935d7313f9637
Caitlin Trussell <caitlintrussell@gmail.com>
2018-02-24 13:36:30 -0600

    changes

--- js/app.js
+++ js/app.js
@@ -3,8 +3,20 @@
 $(document).ready(function () {
 
   $('.blog').click(function () {
-    $('#content').load('./posts/post.html');
-    $('#content').toggle();
+    $('#content').load('./pages/post.html');
+
+      if ($('#content').is(":hidden")) {
+        $('#content').slideDown();
+      }
+      else {
+        $('#content').slideUp();
+        $('.about-content').slideDown();
+      }
 });
+  $('.about').click(function () {
+    $('#content').load('./pages/about.html');
+    $('.about-content').slideToggle();
+
+  })
 
 })
