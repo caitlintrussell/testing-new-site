597dcafb67829ebe6898acda8d72ab094603ddb5
Caitlin Trussell <caitlintrussell@gmail.com>
2018-02-23 20:20:41 -0600

    changed

--- js/app.js
+++ js/app.js
@@ -4,9 +4,7 @@ $(document).ready(function () {
 
   $('.blog').click(function () {
     $('#content').load('./posts/post.html');
+    $('#content').toggle();
 });
-//   $('.blog').on('click', function () {
-//     console.log('hello world')
-//     $('#content').toggle();
-//   })
+
 })
