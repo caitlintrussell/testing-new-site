baeb86c9065c7942640a2ba2848a3fda80d204e8
Caitlin Trussell <caitlintrussell@gmail.com>
2018-02-24 17:18:04 -0600

    jquery fixes

--- js/app.js
+++ js/app.js
@@ -4,33 +4,31 @@ $(document).ready(function () {
   $('.blog').click(function () {
       $('.blog-content').load('./pages/post.html');
       $('.home-img').hide();
-      $('.blog').off('click');
   });
 
-  $('.blog').click(function () {
-    if ($('.blog-content').is(':hidden')) {
-      $('.blog-content').slideDown();
-    }
-    else {
-      $('.blog-content').slideUp();
-      $('.home-img').show();
-    }
-  });
+  // $('.blog').click(function () {
+  //   if ($('.blog-content').is(':hidden')) {
+  //     $('.blog-content').slideDown();
+  //   }
+  //   else {
+  //     $('.blog-content').slideUp();
+  //     $('.home-img').show();
+  //   }
+  // });
 
   $('.about').click(function () {
     $('.about-content').load('./pages/about.html');
     $('.home-img').hide();
-    $('.about').off('click');
 });
 
-$('.about').click(function () {
-  if ($('.about-content').is(':hidden')) {
-    $('.about-content').slideDown();
-  }
-  else {
-    $('.about-content').slideUp();
-    $('.home-img').show();
-  }
-});
+// $('.about').click(function () {
+//   if ($('.about-content').is(':hidden')) {
+//     $('.about-content').slideDown();
+//   }
+//   else {
+//     $('.about-content').slideUp();
+//     $('.home-img').show();
+//   }
+// });
 
 })
