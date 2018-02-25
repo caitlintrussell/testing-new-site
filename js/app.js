7ea90a5aee62e5aa707e759d03893f8a56fdff11
Caitlin Trussell <caitlintrussell@gmail.com>
2018-02-24 19:11:53 -0600

    jquery fixes

--- js/app.js
+++ js/app.js
@@ -1,8 +1,10 @@
 
 $(document).ready(function () {
+  /* Loads in the content for each page */
   $('.blog-content').load('./pages/post.html');
   $('.about-content').load('./pages/about.html')
 
+  /* click event to show the blog */
   $('.blog').click(function () {
     $('#plant-left').show();
     $('#plant-right').show();
@@ -11,7 +13,7 @@ $(document).ready(function () {
     $('.blog-content').show();
 
   });
-
+/* click event to show the about page */
   $('.about').click(function () {
     $('.home-img').hide();
     $('#plant-left').show();
@@ -19,5 +21,36 @@ $(document).ready(function () {
     $('.blog-content').hide();
     $('.about-content').show();
   });
+
+    var didScroll;
+    var lastScrollTop = 0;
+    var delta = 5;
+    var navbarHeight = 120;
+
+  $(window).scroll(function(event){
+    didScroll = true;
+  });
+
+  setInterval(function() {
+    if (didScroll) {
+      hasScrolled();
+      didScroll = false;
+    }
+  }, 250);
+
+  function hasScrolled() {
+    var st = $(this).scrollTop();
+    console.log(this);
+    if (Math.abs(lastScrollTop - st) <= delta) return;
+
+    if (st > lastScrollTop && st > navbarHeight) {
+      $('#header').removeClass('nav-down').addClass('nav-up');
+    }
+    else if (st + $(window).height() < $(document).height()) {
+        $('#header').removeClass('nav-up').addClass('nav-down');
+      }
+      lastScrollTop = st;
+    }
+
 
 })
