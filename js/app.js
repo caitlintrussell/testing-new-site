d0b0f243d96a6742fa8929850acdb5f29de50b1e
Caitlin Trussell <caitlintrussell@gmail.com>
2018-02-24 18:10:16 -0600

    jquery fixes

--- js/app.js
+++ js/app.js
@@ -3,27 +3,21 @@ $(document).ready(function () {
   $('.blog-content').load('./pages/post.html');
   $('.about-content').load('./pages/about.html')
 
-  var blogClicks = false;
   $('.blog').click(function () {
-    if (!blogClicks) {
-      $('.home-img').hide();
-      $('.about-content').hide();
-      $('.blog-content').show();
-      blogClicks = true;
-    }
+    $('#plant-left').show();
+    $('#plant-right').show();
+    $('.home-img').hide();
+    $('.about-content').hide();
+    $('.blog-content').show();
 
   });
 
-
-var aboutClicks = false;
   $('.about').click(function () {
-    if (!aboutClicks) {
-      $('.home-img').hide();
-      $('.blog-content').hide();
-      $('.about-content').show();
-      aboutClicks = true;
-    }
-
-});
+    $('.home-img').hide();
+    $('#plant-left').show();
+    $('#plant-right').show();
+    $('.blog-content').hide();
+    $('.about-content').show();
+  });
 
 })
