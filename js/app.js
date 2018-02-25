5dabf5bcb26fc01475e0ae24a18e6420b3364637
Caitlin Trussell <caitlintrussell@gmail.com>
2018-02-24 21:12:28 -0600

    added media queries

--- js/app.js
+++ js/app.js
@@ -22,41 +22,6 @@ $(document).ready(function () {
     $('.about-content').show();
   });
 
-  var didScroll;
-  var lastScrollTop = 0;
-  var delta = 2;
-  var navbarHeight = $('header').outerHeight();
 
-  $(window).scroll(function(event){
-      didScroll = true;
-  });
 
-  setInterval(function() {
-      if (didScroll) {
-          hasScrolled();
-          didScroll = false;
-      }
-  }, 250);
-
-  function hasScrolled() {
-      var st = $(this).scrollTop();
-
-      // Make sure they scroll more than delta
-      if(Math.abs(lastScrollTop - st) <= delta)
-          return;
-
-      // If they scrolled down and are past the navbar, add class .nav-up.
-      // This is necessary so you never see what is "behind" the navbar.
-      if (st > lastScrollTop && st > 50){
-          // Scroll Down
-          $('header').removeClass('nav-down').addClass('nav-up');
-      } else {
-          // Scroll Up
-          if(st + $(window).height() < $(document).height()) {
-              $('header').removeClass('nav-up').addClass('nav-down');
-          }
-      }
-
-      lastScrollTop = st;
-  }
 })
